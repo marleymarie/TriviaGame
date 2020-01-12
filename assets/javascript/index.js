af522eb7c08e1eb489bd4b1003c6aded944d1fc1
Diamond Gaines <poppijewel@gmail.com>
2020-01-11 22:43:21 -0700

    added the questions to my trivia game

---
+++ assets/javascript/index.js
@@ -0,0 +1,10 @@
+var questions = [
+    { prompt: "what color are apples?\n(a) Red \n(b) Green\n(c) purple",
+    answer: "a"
+    }];
+
+    var questions = ["what color are apples?",
+                        answer: "a"];
+
+    prompt = questions
+ 
